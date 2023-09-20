a79095210942616b6af991d4d1260f7035d10ea2
Jahquan <jthomas052192@gmail.com>
2023-09-20 13:56:19 -0400

    updates

--- input.py
+++ input.py
@@ -1,4 +1,4 @@
-import os
+import os, time
 
 input_folder = '/Users/jahquanthomas/Documents/GitHub/input/text'
 
@@ -6,8 +6,9 @@ for file_name in os.listdir(input_folder):
         input_path = os.path.join(input_folder, file_name)
     
         if os.path.isfile(input_path):
-            print(f"Lines from '{file_name}':")
+            print("Lines from '{}':".format(file_name))
             with open(input_path, "r") as file:
                 for line in file:
                     print(line.strip())
+                    time.sleep(1)
             print("\n")
